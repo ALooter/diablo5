afb777136497ea78035ace8fd47b7e438a587a46
ALooter <58535072+ALooter@users.noreply.github.com>
2020-05-10 20:33:58 +0300

    added main menu

---
+++ Assets/Scripts/UI/mainmenuscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenuscript : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
