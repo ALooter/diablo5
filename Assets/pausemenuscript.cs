703454a12badb30e6fdf8b3cf8ec5dc88b4a2ed2
ALooter <58535072+ALooter@users.noreply.github.com>
2020-05-11 19:21:19 +0300

    pause menu

---
+++ Assets/pausemenuscript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pausemenuscript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseui;
+
+    //private void Start()
+    //{
+    //    pauseui.SetActive(false);
+    //}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseui.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseui.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        Resume();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
