c585a64716d01c7a9a7cc5253137b5ea843c9486
ALooter <58535072+ALooter@users.noreply.github.com>
2020-05-11 20:43:31 +0300

    npc 0.1

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Interactable
+{
+
+    DialogTrigger dialogtrigger;
+
+    void Start()
+    {
+        dialogtrigger = GetComponent<DialogTrigger>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        dialogtrigger.TriggerDialog();
+
+    }
+}
